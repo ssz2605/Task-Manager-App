9309d8a749d3ee3c8eac64cb09b8c1f23e371a43
Shreya Saxena <shreyasaxena2605@gmail.com>
2025-09-03 22:25:37 +0530

    updated .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; 
+import { getFirestore } from "firebase/firestore";
 
-// Firebase config (only appId taken from .env)
 const firebaseConfig = {
-  apiKey: "AIzaSyDkA6Tt1OH4_kbBw8qoCuT4nckAlOAFlKM",
-  authDomain: "task-manager-app-ba470.firebaseapp.com",
-  projectId: "task-manager-app-ba470",
-  storageBucket: "task-manager-app-ba470.firebasestorage.app",
-  messagingSenderId: "406202636571",
-  appId: import.meta.env.VITE_APP_ID,  // taken from env
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
